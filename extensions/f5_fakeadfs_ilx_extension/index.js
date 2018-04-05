6cfd25f43e8b10635c1a0bd02e41be1c5d8cba72
Michael <stumpj@gmail.com>
2018-04-05 09:18:06 -0400

    big_ip_git

---
+++ extensions/f5_fakeadfs_ilx_extension/index.js
@@ -0,0 +1,107 @@
+// Import the f5-nodejs module.
+var f5 = require('f5-nodejs');
+
+//server.js
+// Fake ADFS
+// WS-Federation IDP
+// Michael Coleman
+//
+// Some things are hardcoded for testing purposes while i develop and test.
+
+var path = require('path');
+var jsonQuery = require('json-query');
+var config = require('read-config')(path.join(__dirname, 'config.json'));
+var queryString = require('querystring');
+var fs = require('fs');
+var moment = require('moment');
+var url = require('url');
+
+var timeout = config.federation.timeout;
+
+// Federation Values
+var wsfedIssuer = config.federation.issuer;
+
+var SigningCert = fs.readFileSync(path.join(__dirname, config.federation.certs.tokensigningcert));
+var SigningKey = fs.readFileSync(path.join(__dirname, config.federation.certs.tokensigningkey));
+
+var wsfed = require('./templates').WSFed;
+
+/* These are for IDP initated SSO requets, since the Querystring will be
+   blank.
+   */
+
+var idp_WA = 'signin1.0'
+var idp_WTRealm = 'urn:sharepoint:f5lab'
+var idp_WCTX = ''
+
+// Create a new rpc server for listening to TCL iRule calls.
+var ilx = new f5.ILXServer();
+
+ilx.addMethod('Generate-WSFedToken', function(req, res) {
+try {
+    /* Extract the ILX parameters to add to the Assertion data
+       req.params()[0] is the first passed argument
+       req.params()[1] is the second passed argument, and so on.
+    */
+
+  var query = queryString.unescape(req.params()[0])
+  var queryOptions = queryString.parse(query)
+  var AttrUserName = req.params()[1]
+  var AttrUserPrincipal = req.params()[2]
+
+      /* If incoming request is IDP initiated, the Querystrings will not
+       be populated, so lets check, and if undefined, populate with static
+       IDP config vars.
+       */
+  var wa = queryOptions.wa
+  if (typeof wa === 'undefined') {
+    wa = idp_WA
+  }
+  var wtrealm = queryOptions.wtrealm
+  if (typeof wtrealm === 'undefined') {
+    wtrealm = idp_WTRealm
+  }
+  var wctx = queryOptions.wctx
+  if (typeof wctx === 'undefined') {
+    wctx = idp_WCTX
+  }
+
+  var relyingpartners = config.federation
+  var EndPointfilter = jsonQuery('relyingpartners[name=' + wtrealm + '].options.endpoints.url', { data: relyingpartners})
+  var endPoint = EndPointfilter.value
+
+    /* Generate WSFed Assertion.  These attributes are
+       configured previously in the code.
+       cert: this is the cert used for encryption
+       key: this is the key used for the cert
+       issuer: the assertion issuer
+       lifetimeInSeconds: timeout
+       audiences: this is the application ID for sharepoint, urn:sharepoint:webapp
+       attributes:  these should map to the mappings created for the IDP in SharePoint
+       */
+
+  var wsfed_options = {
+    wsaAddress: wtrealm,
+    cert: SigningCert,
+    key: SigningKey,
+    issuer: wsfedIssuer,
+    lifetimeInSeconds: timeout,
+    audiences: wtrealm,
+    attributes: {
+      'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress': AttrUserName,
+      'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn': AttrUserPrincipal
+    }
+  }
+      /* Sign the Assertion */
+  var signedAssertion = wsfed.create(wsfed_options)
+
+  } catch (e) {
+    res.reply("error: " + e.message + ", stack: " + e.stack); 
+    return;
+  }
+  res.reply(signedAssertion);
+})
+
+// Start listening for ILX::call and ILX::notify events.
+ilx.listen()
+
---
+++ rules/f5_fakeadfs_ilx_single_vs.tcl
@@ -0,0 +1,158 @@
+when HTTP_REQUEST {
+  # save hostname for use in response
+  set fqdn_name [HTTP::host]
+  
+    switch -glob [string tolower [HTTP::path]] {
+        "*/federationmetadata/2007-06/federationmetadata.xml" {
+            #Act as the STS Metadataprovider
+            set fakeadfs_preapm [ILX::init f5_fakeadfs_ilx_extension]
+            set issuer "https://[HTTP::host]"
+            set endpoint "[HTTP::uri]"
+            set metadata_response [ILX::call $fakeadfs_preapm Generate-FederationMetadata $issuer $endpoint]
+            HTTP::respond 200 content $metadata_response Content-Type "application/xml"
+        }
+        "*/STS/Login.aspx*" {
+            #Act as STS Login Provider
+            #  WIF applications will do a passive redirect to STS to auth.
+            #  Maybe let APM handle this landinguri.
+
+        }
+        "*adfs/services/trust/*" {
+            #Act as WS-Trust Provider
+            #Path can determine Auth Type, I dont want to code all of these so will have to pick a few important ones.
+            #  *adfs/services/trust/*/certificate
+            #  *adfs/services/trust/*/username
+            #  *adfs/services/trust/*/issuedtoken*
+        }
+        "*/adfs/ls*" {
+            log local0. ".../adfs/ls path..."
+            #  Act as WS-Federation Provider
+            #  Wctx: This is some session data that the application wants sent back to 
+            #  it after the user authenticates.
+            set wctx [URI::decode [URI::query [HTTP::uri] wctx]]
+            #  Wa=signin1.0: This tells the ADFS server to invoke a login for the user.
+            set wa [URI::decode [URI::query [HTTP::uri] wa]]
+            #  Wtrealm: This tells ADFS what application I was trying to get to. 
+            #  This has to match the identifier of one of the relying party trusts 
+            #  listed in ADFS.  wtrealm is used in the Node.JS side, but we dont need it 
+            #  here.
+
+            #  Kept getting errors from APM, this fixed it.
+            node 127.0.0.1    
+            
+            #  Make sure that the user has authenticated and APM has created a session.
+            if {[HTTP::cookie exists MRHSession] } {
+        
+                #log local0. "Generate POST form and Autopost "
+        
+                #  tmpresponse is the WS-Fed Assertion data, unencoded, so straight XML
+                set tmpresponse [ACCESS::session data get session.custom.idam.wsfedtoken]
+        
+                #  This was the pain to figure out.  The assertion has to be POSTed to 
+                #  SharePoint, this was the easiest way to solve that issue.  Set timeout
+                #  to half a second, but can be adjusted as needed.
+                set htmltop "<html><script type='text/javascript'>window.onload=function(){ window.setTimeout(document.wsFedAuth.submit.bind(document.wsFedAuth), 500);};</script><body>"
+                set htmlform "<form name='wsFedAuth' method=POST action='https://sharepoint.f5lab.com/_trust/default.aspx?trust=FakeADFS'><input type=hidden name=wa value=$wa><input type=hidden name=wresult value='$tmpresponse'><input type=hidden name=wctx value=$wctx><input type='submit' value='Continue'></form/>"
+                set htmlbottom "</body></html>"
+                set page "$htmltop $htmlform $htmlbottom"
+                
+                HTTP::respond 200 content $page
+            }
+        }
+        default {
+        log local0. "::Default path::"
+        #  Wctx: This is some session data that the application wants sent back to 
+        #  it after the user authenticates.
+        set wctx [URI::decode [URI::query [HTTP::uri] wctx]]
+        #  Wa=signin1.0: This tells the ADFS server to invoke a login for the user.
+        set wa [URI::decode [URI::query [HTTP::uri] wa]]
+        #  Wtrealm: This tells ADFS what application I was trying to get to. 
+        #  This has to match the identifier of one of the relying party trusts 
+        #  listed in ADFS.  wtrealm is used in the Node.JS side, but we dont need it 
+        #  here.
+        
+        #log local0. "Cookie: [HTTP::cookie MRHSession]"
+        #log local0. "Cookie: [HTTP::cookie FedAuth]"
+        #log local0. "Method: [HTTP::method]"
+        #log local0. "wsFed:  [ACCESS::session data get session.custom.idam.wsfedtoken]"
+        
+            if {[HTTP::cookie exists MRHSession] && 
+                ([HTTP::method] ne "POST") &&
+                not ( [HTTP::cookie exists FedAuth] ) &&
+                [ACCESS::session data get session.custom.idam.wsfedtoken] ne ""} {
+                node 127.0.0.1
+                log local0. "...MRHSession && WSFedToken Exist, FedAuth Cookie Does Not... "
+
+                #  tmpresponse is the WS-Fed Assertion data, unencoded, so straight XML
+                set tmpresponse [ACCESS::session data get session.custom.idam.wsfedtoken]
+        
+                #  This was the pain to figure out.  The assertion has to be POSTed to 
+                #  SharePoint, this was the easiest way to solve that issue.  Set timeout
+                #  to half a second, but can be adjusted as needed.
+                set currentHost [HTTP::host]
+                set htmltop "<html><script type='text/javascript'>window.onload=function(){ window.setTimeout(document.wsFedAuth.submit.bind(document.wsFedAuth), 500);};</script><body>"
+                set htmlform "<form name='wsFedAuth' method=POST action='https://$currentHost/_trust/default.aspx?trust=FakeADFS'><input type=hidden name=wa value=$wa><input type=hidden name=wresult value='$tmpresponse'><input type=hidden name=wctx value=$wctx><input type='submit' value='Continue'></form/>"
+                set htmlbottom "</body></html>"
+                set page "$htmltop $htmlform $htmlbottom"
+                
+                HTTP::respond 200 content $page
+                }
+            }
+    }
+}
+
+when ACCESS_POLICY_AGENT_EVENT {
+    #  Create the ILX RPC Handler
+    set fakeadfs_handle [ILX::init f5_fakeadfs_plugin f5_fakeadfs_ilx_extension]
+    #  Payload is just the incoming Querystring
+    set payload [ACCESS::session data get session.server.landinguri]
+    #  Currently, the mapped attributes are Email & UPN.  In some environments,
+    #  this may match, for my use case, they will not, so there is an LDAP AAA
+    #  which is queried based on the logon name (email), and the UPN is retrieved
+    #  from LDAP.
+    set AttrUserName [ACCESS::session data get session.logon.last.username]
+    set AttrUserPrin [ACCESS::session data get session.ldap.last.attr.userPrincipalName ]
+
+    #  Current solution uses Node.JS SAML module and can support SAML11, as well
+    #  as SAML20.  The APM policy calls the irule even ADFS, with generates the token
+    #  based on the submitted QueryString and the logon attributed.
+    switch [ACCESS::policy agent_id] { 
+               "ADFS" {
+                    log local0. "Received Process request for FakeADFS, $AttrUserName, $AttrUserPrin, $payload"
+                    set wsfed_response [ILX::call $fakeadfs_handle Generate-WSFedToken $payload $AttrUserName $AttrUserPrin]
+                    ACCESS::session data set session.custom.idam.wsfedtoken $wsfed_response  
+               }
+    }
+}
+
+#  This may or may not be needed, they arent populated with actual values, but 
+#  have not tested WITHOUT yet.
+#
+#  MSISAuth and MSISAuth1 are the encrypted cookies used to validate the SAML 
+#  assertion produced for the client. These are what we call the "authentication 
+#  cookies", and you will see these cookies ONLY when AD FS 2.0 is the IDP. 
+#  Without these, the client will not experience SSO when AD FS 2.0 is the IDP.
+#
+#  MSISAuthenticated contains a base64-encoded timestamp value for when the client 
+#  was authenticated. You will see this cookie set whether AD FS 2.0 is the IDP 
+#  or not.
+#
+#  MSISSignout is used to keep track of the IDP and all RPs visited for the SSO 
+#  session. This cookie is utilized when a WS-Federation sign-out is invoked. 
+#  You can see the contents of this cookie using a base64 decoder.
+#  MSISLoopDetectionCookie is used by the AD FS 2.0 infinite loop detection 
+#  mechanism to stop clients who have ended up in an infinite redirection loop 
+#  to the Federation Server. For example, if an RP is having an issue where it 
+#  cannot consume the SAML assertion from AD FS, the RP may continuously redirect 
+#  the client to the AD FS 2.0 server. When the redirect loop hits a certain 
+#  threshold, AD FS 2.0 uses this cookie to detect that threshold being met, 
+#  and will throw an exception which lands the user on the AD FS 2.0 error page 
+#  rather than leaving them in the loop. The cookie data is a timestamp that is 
+#  base64 encoded.
+when ACCESS_ACL_ALLOWED {
+    HTTP::cookie insert name "MSISAuth" value "ABCD" path "/adfs"
+    HTTP::cookie insert name "MSISSignOut" value "ABCD" path "/adfs"
+    HTTP::cookie insert name "MSISAuthenticated" value "ABCD" path "/adfs"
+    HTTP::cookie insert name "MSISLoopDetectionCookie" value "ABCD" path "/adfs"
+}
+
